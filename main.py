eb5b8a4339d9721ed99d3cb3afdbf3ff5337528e
Anton Kolot <antvelkol@gmail.com>
2022-12-05 10:55:32 +0200

    init main file

---
+++ main.py
@@ -0,0 +1,8 @@
+# My goal is to make the game tic-tac-toe scalable - without using hard code - so that you can change the parameters
+# by changing the playing field.
+#
+# At the same time, I have immutable rules that the user cannot change, such as the name "X" and "O", and also that
+# the number of cells is equal in width and height.
+#
+# It is also ideal to write the code typed so that it is easy to read and understand.
+

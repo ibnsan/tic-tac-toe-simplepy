74f25a0ade1d89b9060130a472dbd3c5b5b6a11a
Anton Kolot <antvelkol@gmail.com>
2022-12-29 21:09:40 +0200

    Added a function to get the width of the playing board

--- main.py
+++ main.py
@@ -8,7 +8,22 @@
 import os
 
 demo_board = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x']
-board_width = 3
+
+
+def get_board_length():
+    '''
+    Asks the user for the width of the board in numbers and returns that width
+    :return: Playing board width in numbers above 2
+    '''
+    length = 0
+    while not (isinstance(int(length), int) and int(length) > 2 and int(length) < 24):
+        length = input(
+            "Specify the length of the playing board (the length must be in numbers and greater than 2): ")
+
+    return int(length)
+
+
+board_width = get_board_length()
 
 
 def display_board(board, line_width):
@@ -34,5 +49,3 @@ def display_board(board, line_width):
 
 board = display_board(demo_board, board_width)
 print(board)
-
-

4448eba4b764aafa625943b3ed1a25a52f2e4b03
Anton Kolot <antvelkol@gmail.com>
2023-01-10 09:58:00 +0200

    Added a function for the user to select a symbol

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ demo_board = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x']
 
 
 def get_board_width():
-    '''
+    """
     Asks the user for the width of the board in numbers and returns that width
     :return: Playing board width in numbers above 2
-    '''
+    """
     width = 0
     while not (isinstance(int(width), int) and int(width) > 2 and int(width) < 24):
         width = input(
@@ -27,10 +27,10 @@ board_width = get_board_width()
 
 
 def create_board(board_width):
-    '''
+    """
     :param board_width: Playing board width in numbers
     :return: Playboard array where each cell corresponds to an index
-    '''
+    """
     board_size = board_width * board_width
     board = []
     for cell_number in range(board_size):
@@ -38,15 +38,31 @@ def create_board(board_width):
     return board
 
 
+def select_symbol():
+    """
+    :return: Selected user characters (x or y)
+    """
+    marker = ''
+    while not (marker == "X" or marker == "O"):
+        marker = input("Player 1 - please choose you marker 'x or o': ").upper()
+
+    if marker == "X":
+        return ("x", "o")
+    else:
+        return ("o", "x")
+
+
+player1_marker, player2_marker = select_symbol()
+
 game_board = create_board(board_width)
 
 
 def display_board(board, line_width):
-    '''
+    """
     :param board: An array of element values - the number of which is a multiple of line_width
     :param line_width: Board length horizontally and vertically
     :return: Board with separator | with the number of characters in width and height equal to line_width
-    '''
+    """
     os.system('cls')
 
     current_line = "|"
